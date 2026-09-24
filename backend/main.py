08af4c261e7249e5713ae9251daea21bf36511d4
Srajan Kharvi <srajankharvi.dev@gmail.com>
2026-09-24 22:20:10 +0530

    Add Vercel URL to CORS origins

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         settings.FRONTEND_URL,
+        "https://trade-analysis-eta.vercel.app",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000",
